573345d8d0834d9ffa14cf13ac023c9d0a1ab67f
Ly <tngmengly@gmail.com>
2021-07-20 14:51:04 +0700

    button binding, display

--- Calculator_gui.py
+++ Calculator_gui.py
@@ -2,33 +2,67 @@ from tkinter import *
 
 root = Tk()
 
+
+# functions
+def d0(text):
+    num_input.insert(END, text)
+def d1(text):
+    num_input.insert(END, text)
+def d2(text):
+    num_input.insert(END, text)
+def d3(text):
+    num_input.insert(END, text)
+def d4(text):
+    num_input.insert(END, text)
+def d5(text):
+    num_input.insert(END, text)
+def d6(text):
+    num_input.insert(END, text)
+def d7(text):
+    num_input.insert(END, text)
+def d8(text):
+    num_input.insert(END, text)
+def d9(text):
+    num_input.insert(END, text)
+def dp(text):
+    num_input.insert(END, text)
+def dd(text):
+    num_input.insert(END, text)
+def dm(text):
+    num_input.insert(END, text)
+def ds(text):
+    num_input.insert(END, text)
+def dde():
+    num_input.delete(0, END)
+def de():
+    num_input.delete(0, END)
+
 num_input = Entry(root, width=40, borderwidth=5)
 num_input.grid(row=0, column=0, columnspan=4, padx=7, pady=5)
 
-# functions
 # add functionalities here...
 
 # Buttons
 
-btn1 = Button(root, text="1", padx=40, pady=20)
-btn2 = Button(root, text="2", padx=40, pady=20)
-btn3 = Button(root, text="3", padx=40, pady=20)
+btn1 = Button(root, text="1", padx=40, pady=20, command=lambda: d1("1"))
+btn2 = Button(root, text="2", padx=40, pady=20, command=lambda: d2("2"))
+btn3 = Button(root, text="3", padx=40, pady=20, command=lambda: d3("3"))
 
-btn4 = Button(root, text="4", padx=40, pady=20)
-btn5 = Button(root, text="5", padx=40, pady=20)
-btn6 = Button(root, text="6", padx=40, pady=20)
+btn4 = Button(root, text="4", padx=40, pady=20, command=lambda: d4("4"))
+btn5 = Button(root, text="5", padx=40, pady=20, command=lambda: d5("5"))
+btn6 = Button(root, text="6", padx=40, pady=20, command=lambda: d6("6"))
 
-btn7 = Button(root, text="7", padx=40, pady=20)
-btn8 = Button(root, text="8", padx=40, pady=20)
-btn9 = Button(root, text="9", padx=40, pady=20)
-btn0 = Button(root, text="0", padx=40, pady=20)
+btn7 = Button(root, text="7", padx=40, pady=20, command=lambda: d7("7"))
+btn8 = Button(root, text="8", padx=40, pady=20, command=lambda: d8("8"))
+btn9 = Button(root, text="9", padx=40, pady=20, command=lambda: d9("9"))
+btn0 = Button(root, text="0", padx=40, pady=20, command=lambda: d0("0"))
 
-btn_plus = Button(root, text="+", padx=37, pady=20)
-btn_minus = Button(root, text="-", padx=40, pady=20)
-btn_mul = Button(root, text="*", padx=40, pady=20)
-btn_div = Button(root, text="/", padx=40, pady=20)
-btn_equal = Button(root, text="=", padx=40, pady=20)
-btn_clear = Button(root, text="C", padx=40, pady=20, fg="red")
+btn_plus = Button(root, text="+", padx=37, pady=20, command=lambda: dp("+"))
+btn_minus = Button(root, text="-", padx=40, pady=20, command=lambda: ds("-"))
+btn_mul = Button(root, text="*", padx=40, pady=20, command=lambda: dm("×"))
+btn_div = Button(root, text="/", padx=40, pady=20, command=lambda: dd("/"))
+btn_equal = Button(root, text="=", padx=40, pady=20, command=de)
+btn_clear = Button(root, text="C", padx=40, pady=20, fg="red", command=dde)
 
 # Buttons layout
 
